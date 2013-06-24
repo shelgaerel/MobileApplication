331835c0c574f9cef0fe79ed75c6a114c315a9d3
matteo navanzino <matteo.navanzino@gmail.com>
2013-06-25 01:01:55 +0200

    Inizio del Merge

---
+++ CoreLibrary/Utility/MobileOptions.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace CoreLibrary
+{
+	public static class MobileOptions
+	{
+		private static string DataBaseFilePath = "ProgettoPilota.db";
+
+		static public string GetDataBaseFilePath()
+		{
+			#if SILVERLIGHT
+			// Windows Phone expects a local path, not absolute
+			var path = sqliteFilename;
+			#else
+
+			#if __ANDROID__
+			// Just use whatever directory SpecialFolder.Personal returns
+			string libraryPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal); ;
+			#else
+			// we need to put in /Library/ on iOS5.1 to meet Apple's iCloud terms
+			// (they don't want non-user-generated data in Documents)
+			string documentsPath = Environment.GetFolderPath (Environment.SpecialFolder.Personal); // Documents folder
+			string libraryPath = Path.Combine (documentsPath, "../Library/"); // Library folder
+			#endif
+			var path = Path.Combine (libraryPath, DataBaseFilePath);
+			#endif	
+			return path;
+		}
+	}
+}
+
